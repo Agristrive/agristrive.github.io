a379a35eea27ed77b259e3002f38a996851eaa78
Agristrive <agristrivetsa@gmail.com>
2025-04-01 14:53:50 -0400

    Update reader.js

--- reader.js
+++ reader.js
@@ -165,6 +165,9 @@ function check_cookie() {
 
 function on_start(){
     check_cookie()
+
+    document.addEventListener('contextmenu', event => event.preventDefault())
+    
     let wait_cookie = setInterval(() => {
         if (document.body.getAttribute("cookied_user")){
             log_in(document.body.getAttribute("cookied_user"), document.body.getAttribute("cookied_password"))
