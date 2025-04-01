38220edfd73eba81494fd0fb6e43884b22ac9371
Agristrive <agristrivetsa@gmail.com>
2025-03-31 22:34:29 -0400

    Update myfarms.js

--- myfarm/myfarms.js
+++ myfarm/myfarms.js
@@ -450,7 +450,7 @@ function check_account(){
 
 function check_cookie() {
     let cookie_data = document.cookie;
-    let returned = [];
+    let returned = [null, null];
     console.log(cookie_data)
     for (let i = 0; i < cookie_data.split(";").length; i++) {
         let index = cookie_data.split(";")[i].split("=")[0].trim();
